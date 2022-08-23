df8e47c09df070f014de99a3a1ddb0ca7443f489
jr972 <jr972@CharlesXavier>
2022-08-23 14:55:36 -0400

    Implementacion semantic

--- index.php
+++ index.php
@@ -7,10 +7,22 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" type="text/css" href="css/semantic/dist/semantic.min.css">
     </head>
     <body>
+        <button class="ui primary button">
+            Save
+        </button>
+        <button class="ui button">
+            Discard
+        </button>
         <?php
-        // put your code here
+        echo 'Hola';
         ?>
     </body>
+    <script
+        src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+    crossorigin="anonymous"></script>
+    <script src="js/semantic/dist/semantic.min.js"></script>
 </html>
