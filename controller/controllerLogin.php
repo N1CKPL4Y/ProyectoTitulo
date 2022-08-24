b4e48ca020a0949a0b6a761487e698931a7a2fed
N1CKPL4Y <N1CKPL4Y@DESKTOP-BAP1TNN.lan>
2022-08-24 15:40:31 -0400

    codificacion de redirecciones

--- controller/controllerLogin.php
+++ controller/controllerLogin.php
@@ -1,5 +1,97 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Iniciando Sesion</title>
+        <link rel="stylesheet" href="../Materialize/css/styleBody.css"/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <script>
+            function Admin() {
+                swal({
+                    title: "Bienvenido",
+                    text: "Administrador",
+                    type: "success",
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Aceptar"
+                },
+                        function () {
+                            window.location.href = '../index.php';
+                        });
+            }
 
+            function Gerencia() {
+                swal({
+                    title: "Bienvenido",
+                    text: "Gerente",
+                    type: "success",
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Aceptar"
+                },
+                        function () {
+                            window.location.href = '../index.php';
+                        });
+            }
+
+            function Secretaria() {
+                swal({
+                    title: "Bienvenida",
+                    text: "Secretaria",
+                    type: "success",
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Aceptar"
+                },
+                        function () {
+                            window.location.href = '../MenuPrincipal.php';
+                        });
+            }
+
+            function Profesional() {
+                swal({
+                    title: "Bienvenido",
+                    text: "Profesional",
+                    type: "success",
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Aceptar"
+                },
+                        function () {
+                            window.location.href = '../index.php';
+                        });
+            }
+
+            function Practicante() {
+                swal({
+                    title: "Bienvenido",
+                    text: "Practicante",
+                    type: "success",
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Aceptar"
+                },
+                        function () {
+                            window.location.href = '../index.php';
+                        });
+            }
+
+            function Error() {
+                swal({
+                    title: "ERROR",
+                    text: "El R.U.T y/o contraseña incorrectas, intentelo de nuevo",
+                    type: "error",
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Aceptar"
+                },
+                        function () {
+                            window.location.href = '../index.php';
+                        });
+            }
+        </script>
+    </body>
+</html>
+
+
+<?php
 include_once '../Model_Data.php';
 session_start();
 
@@ -25,11 +117,26 @@ if ($rut && $pass) {
             $_SESSION['area_u'] = $key['a_user'];
             $_SESSION['cargo'] = $key['cargo'];
         }
-        
-        echo '<script language="javascript">alert("Bienvenida");window.location.href="../MenuPrincipal.php"</script>';
-        
+
+        //echo '<script language="javascript">alert("Bienvenida");window.location.href="../MenuPrincipal.php"</script>';
+
+        switch ($_SESSION['tipo_u']) {
+            case 1:
+                echo '<script>Admin();</script>';
+            case 2:
+                switch ($_SESSION['cargo']) {
+                    case 1:
+                        echo '<script>Gerencia();</script>';
+                    case 2:
+                        echo '<script>Secretaria();</script>';
+                    case 3:
+                        echo '<script>Profesional();</script>';
+                    case 4:
+                        echo '<script>Practicante();</script>';
+                }
+        }
     } else if (!$valid) {
-        echo 'ERROR';
+        echo '<script>Error();</script>';
     }
 } else {
     header("location: ../index.php");
