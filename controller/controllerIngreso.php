08904469987cd3728803c7febe656b1f8730b53b
jr972 <jr972@CharlesXavier>
2022-09-15 17:35:52 -0300

    datatables

--- controller/controllerIngreso.php
+++ controller/controllerIngreso.php
@@ -26,6 +26,18 @@
             function Error() {
                 swal({
                     title: "ERROR",
+                    text: "Intentelo nuevamente",
+                    type: "error",
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Aceptar"
+                },
+                        function () {
+                            window.location.href = '../MenuSecretaria.php';
+                        });
+            }
+            function ErrorExistencia() {
+                swal({
+                    title: "ERROR",
                     text: "Datos faltantes, incorrectos y/o ya existen en el sistema. Intentelo nuevamente",
                     type: "error",
                     confirmButtonColor: "#DD6B55",
@@ -207,7 +219,13 @@ if (!isset($_FILES["file_tutor"]) || $_FILES["file_tutor"]["error"] > 0) {
 
 $prevision = isset($_POST['cbo_prevision']) ? $_POST['cbo_prevision'] : null;
 $teleton = isset($_POST['rd_teleton']) ? $_POST['rd_teleton'] : null;
-$numeroTeleton = isset($_POST['txt_Nteleton']) ? $_POST['txt_Nteleton'] : null;
+$numeroTeleton;
+
+if ($teleton==1){
+    $numeroTeleton = isset($_POST['txt_Nteleton']) ? $_POST['txt_Nteleton'] : null;
+}else{
+    $numeroTeleton=0;
+}
 
 ////////////////////////////Credencial
 $haveCreden = isset($_POST['discapacidad']) ? $_POST['discapacidad'] : null;
@@ -348,8 +366,9 @@ if (!isset($_FILES["file_Hogar"]) || $_FILES["file_Hogar"]["error"] > 0) {
 
 /////////////////////////////////Insercion de datos///////////////////////////
 
-if ($rut != $existeBene) {
-    if ($rut && $nombre && $apellido && $fecha && $genero && $direccion && $comuna && $dataFile && $teleton && $pension && $chSolid && $hogar && $rutTutor && $nombreTutor && $fecha_tutor && $direTutor && $comuTutor && $carnetTutor && $nivelE && $ocupacion && $telefono && $correoTutor && $prevision && $previBene) {
+if (!$existeBene) {
+    if ($rut && $nombre && $apellido && $fecha && $genero && $direccion && $comuna && $dataFile && $teleton==0 && $pension && $chSolid && $hogar && $rutTutor && $nombreTutor && $fecha_tutor
+            && $direTutor && $comuTutor && $carnetTutor && $nivelE && $ocupacion && $telefono && $correoTutor && $prevision && $previBene) {
         echo '<script language="javascript">Success()</script>';
         //insert datos generales
         
@@ -371,22 +390,20 @@ if ($rut != $existeBene) {
         if ($haveCreden == 1) {
             $data->addCredencialD($numeroCreden, $origenP, $origenS, $porcent, $grado, $movilidad, $credenFileFront, $credenFileBack, $rut);
         } else {
-            
+            echo 'hola';
         }
         //$data->addPensionBene($rut, $pension);
 
         if ($teleton == 1) {
             $data->addTeleton($numeroTeleton, $rut);
-        } else {
-            
         }
 
         $data->addRegisSocial($hogarFile, $tipoDocu, $rut);
     } else {
         echo '<script language="javascript">Error()</script>';
     }
-} else if ($rut == $existeBene) {
-    echo '<script language="javascript">Error()</script>';
+} else if ($existeBene) {
+    echo '<script language="javascript">ErrorExistencia()</script>';
 }
 
 
@@ -394,11 +411,11 @@ if ($rut != $existeBene) {
   echo '<br>' . $rut . " " . $nombre . " " . $apellido . " " . $fecha . " " . $genero . " " . $direccion . " " . $comuna . " " . $teleton . " " . $pensionbd . " " . $chSolid . " " . $hogar . "<br>";
   echo '<br>' . $rut . " " . $pension;
   echo '<br>' . $rutTutor . " " . $nombreTutor . " " . $fecha_tutor . " " . $direTutor . " " . $comuTutor . " " . $nivelE . " " . $ocupacion . " " . $telefono . " " . $correoTutor . " " . $prevision . " ";
-  echo '<br>' . $parentezco . " " . $rut . " " . $rutTutor;
+  /*echo '<br>' . $parentezco . " " . $rut . " " . $rutTutor;
   echo '<br>' . $numeroTeleton . " " . $rut;
   echo '<br>' . $numeroCreden . " " . $origenP . " " . $origenS . " " . $porcent . " " . $grado . " " . $movilidad . " " . $rut;
   echo '<br>' . $especialista . " " . $fecha_control . " " . $rut . " " . $condicion . " " . $tipoArchi;
-  
-  echo '<br>'.$existeBene;
+  */
+  echo '<br>'.$haveCreden;
   //echo '<script language="javascript">alert("Excelente");window.location.href="../MenuSecretaria.php"</script>'; */
 ?>
